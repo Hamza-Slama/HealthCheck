d21a481d833111ca51d0b7009a98fd1fce043f5b
Hamza Slama <hamzaslama8@gmail.com>
2020-11-16 21:54:34 +0100

    add cors conf

--- src/main/java/com/healthcheck/HealthCheck.java
+++ src/main/java/com/healthcheck/HealthCheck.java
@@ -2,11 +2,24 @@ package com.healthcheck;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
 public class HealthCheck {
     public static void main(String[] args) {
         SpringApplication.run(HealthCheck.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+            }
+        };
     }
 }
